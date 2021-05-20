cfa849a535c1f31690dd26a72a10e4b94976b36c
Ivan <ivan.lek123@gmail.com>
2021-05-20 14:00:14 +0300

    red

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-let str = "one two"
-console.log(str.slice(1,5))
-​
-let arr = ["one", "two"]
-arr.splice(0, 1, "three")
-console.log(arr)
-​
-let array = [1,2,3,4]
-​
-​
-for(let i = 0; i<array.length; i++){
-    console.log(array[i])
-}
-​
-​
-array.map(function(value){
-    console.log(`value+1: ${value+1}`)
-​
-} )
-​
-​
-array.forEach((value, index) => {
-    console.log(`index: ${index} value: ${value}`)
-});
-​
-​
-let mas = [
-    {name: "Djon"},
-    {name: "Mark"},
-    {name: "Tom"},
-]
-​
-​
-mas.filter(function(mas){
-    console.log(mas.name)
-})
-​
-​
-let a = array.reduce((sum, item)=>item + sum , 0 )
-console.log(a)
-​
-​
-let arr2 = [11, 1, 5, 22, 21, 3,5,7]
-​
-arr2.sort((a,b) => a - b)
-console.log(arr2)
-​
-​
-console.log(arr2.some((a) => a > 55 ))
-console.log(arr2.some((a) => a > 2 ))
-​
-​
-console.log(arr2.every((a) => a > 20))
-console.log(arr2.every((a) => a > 0))
+//обьявление
+let select = document.querySelector(".form-select");
+let btn = document.querySelector(".btn");
+let input = document.querySelector(".form-control");
+let check = document.querySelector(".gg")
+//показывает в input текст selecta
+select.onchange = function () {
+  let str = select.options[select.selectedIndex].text;
+  input.value = str;
+  console.log(input.getAttribute("value"));
+};
+
+//меняет текст option+проверка на пустую строку
+input.onfocus = function () {
+  input.classList.remove("is-invalid");
+  btn.classList.remove("btn2");
+};
+btn.onclick = function () {
+  if (input.value.trim().length === 0) {
+    input.classList.add("is-invalid");
+    btn.classList.add("btn2")
+    
+    
+  } else if (input.value.trim().length === 1) {
+    input.value = "";
+  } else {
+    select.options[select.selectedIndex].text = input.value;
+  }
+};
